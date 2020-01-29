f17857b319eb3c516e7c92e16ea75beafddfa01f
Olha Virolainen <olga.shukel@gmail.com>
2020-01-29 15:39:34 +0200

    Add unittest

--- spec-integration/makeRequest.spec.js
+++ spec-integration/makeRequest.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const logger = require('@elastic.io/component-logger')();
 
-const action = require('../lib/actions/makeRequest');
+const makeRequest = require('../lib/actions/makeRequest');
 
 const cfg = {
   openApiUrl: 'https://petstore.swagger.io/v2/swagger.json',
@@ -11,65 +11,75 @@ const self = {
   logger,
 };
 
-describe('Get Pets By Status', () => {
-  beforeEach(() => {
-    self.emit = sinon.spy();
-  });
-  afterEach(() => {
-    sinon.restore();
-  });
-  it('getPath test', async () => {
-    const result = await action.getPath.call(self, cfg);
-    expect(result).to.have.ownPropertyDescriptor('/pet');
-  });
-  it('getOperations test', async () => {
-    cfg.path = '/pet';
-    const result = await action.getOperations.call(self, cfg);
-    expect(result).to.have.ownPropertyDescriptor('post');
-  });
-
-  it('getMetaModel test', async () => {
-    cfg.path = '/pet/{petId}';
-    cfg.operation = 'post';
-    const result = await action.getMetaModel.call(self, cfg);
-    expect(result.in).to.deep.equal({
-      type: 'object',
-      properties: {
-        petId: {
-          type: 'integer',
-          required: true,
-        },
-        body: {
-          type: 'object',
-          properties: {},
-        },
-      },
+describe('makeRequest action test', () => {
+  describe('getPath test', () => {
+    it('getPath should succeed', async () => {
+      const result = await makeRequest.getPath.call(self, cfg);
+      expect(result).to.have.ownPropertyDescriptor('/pet');
     });
   });
-  it('process test', async () => {
-    cfg.path = '/pet/{petId}';
-    cfg.operation = 'get';
-    const msg = {
-      body: {
-        petId: 3,
-      },
-    };
-    await action.process.call(self, msg, cfg);
-    expect(self.emit.calledOnce).to.equal(true);
+
+  describe('getOperations test', () => {
+    it('getOperations should succeed', async () => {
+      cfg.path = '/pet';
+      const result = await makeRequest.getOperations.call(self, cfg);
+      expect(result).to.have.ownPropertyDescriptor('post');
+    });
   });
 
-  it('process test with dontThrowErrorFlg', async () => {
-    cfg.path = '/pet/{petId}';
-    cfg.operation = 'get';
-    cfg.dontThrowErrorFlg = true;
-    const msg = {
-      body: {
-        petId: 'eeee',
-      },
-    };
-    await action.process.call(self, msg, cfg);
-    expect(self.emit.calledOnce).to.equal(true);
-    expect(self.emit.firstCall.args[0]).to.equal('data');
-    expect(self.emit.firstCall.args[1].body.statusCode).to.equal(404);
+  describe('getMetaModel test', () => {
+    it('getMetaModel should succeed', async () => {
+      cfg.path = '/pet/{petId}';
+      cfg.operation = 'post';
+      const result = await makeRequest.getMetaModel.call(self, cfg);
+      expect(result.in).to.deep.equal({
+        type: 'object',
+        properties: {
+          petId: {
+            type: 'integer',
+            required: true,
+          },
+          body: {
+            type: 'object',
+            properties: {},
+          },
+        },
+      });
+    });
+  });
+
+  describe('makeRequest action process test', () => {
+    beforeEach(() => {
+      self.emit = sinon.spy();
+    });
+    afterEach(() => {
+      sinon.restore();
+    });
+    it('process test', async () => {
+      cfg.path = '/pet/{petId}';
+      cfg.operation = 'get';
+      const msg = {
+        body: {
+          petId: 3,
+        },
+      };
+      await makeRequest.process.call(self, msg, cfg);
+      expect(self.emit.calledOnce).to.equal(true);
+    });
+
+    it('process test with dontThrowErrorFlg', async () => {
+      cfg.path = '/pet/{petId}';
+      cfg.operation = 'get';
+      cfg.dontThrowErrorFlg = true;
+      const msg = {
+        body: {
+          petId: 'eeee',
+        },
+      };
+      await makeRequest.process.call(self, msg, cfg);
+      expect(self.emit.calledOnce).to.equal(true);
+      expect(self.emit.firstCall.args[0]).to.equal('data');
+      expect(self.emit.firstCall.args[1].body.responseCode).to.equal(404);
+    });
   });
 });
