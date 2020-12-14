16d9a84ca894eff9d3c4c3ec90785d337535d6a6
Olha Virolainen <olga.virolainen@elastic.io>
2020-12-14 17:54:56 +0200

    Update sailor 2.6.21 (#5)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.1.1 (December 18, 2020)
+
+* Update Sailor version to 2.6.21
+* Annual audit of the component code to check if it exposes a sensitive data in the logs
+* Annual npm vulnerabilities audit
+
 ## 1.1.0 (February 13, 2020)
 
 * Support OAuth2 authentication type
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,8 +1,9 @@
 const { OpenApiClient } = require('./lib/openApiClient');
 
 module.exports = async function verify(credentials) {
-  this.logger.debug('credentials:', JSON.stringify(credentials));
+  this.logger.info('Starting credentials verification');
   const openApiClient = new OpenApiClient(this, credentials);
   await openApiClient.init();
+  this.logger.info('Verification completed successfully');
   return true;
 };
